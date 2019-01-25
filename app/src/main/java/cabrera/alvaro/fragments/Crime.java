7af837577f43dccb8dc7d249abaec1e38d923a2e
Alvaro <dam2a05@SINF2003SRVR2.LOCAL>
2019-01-25 20:52:34 +0100

    Initial commit

---
+++ app/src/main/java/cabrera/alvaro/fragments/Crime.java
@@ -0,0 +1,43 @@
+package cabrera.alvaro.fragments;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    // Atributes
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    // Constructor
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    // Methods
+    public UUID getId() {
+        return mId;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+    public Date getDate() {
+        return mDate;
+    }
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    public boolean isSolved() {
+        return mSolved;
+    }
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
